4e922324298504bec6154c6e22bb62ecb69fc1f7
its-justus <ianjohnson9042@gmail.com>
2020-06-07 22:02:13 -0500

    modal form created

--- server/public/client.js
+++ server/public/client.js
@@ -1,3 +1,15 @@
-// $('document').ready(() => {
-//     $('.collapse').collapse();
-// })
+$('document').ready(() => {
+    
+    // click handlers
+    $('#modalAddTaskSubmit').on('click', addTask);
+})
+
+
+/**
+ * addTask creates a task object from form data and sends an ajax
+ * request to the server. it then reloads the task lists
+ * 
+ * @param params
+ * @returns returns
+ */
+function
