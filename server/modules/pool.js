2fa854a545970da163b882963ae7d38cfa8549f6
its-justus <ianjohnson9042@gmail.com>
2020-06-09 16:49:56 -0500

    added heroku compatibility

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,15 +1,36 @@
 // imports
-const pg = require('pg'); // postgres db driver
+const pg = require('pg');
+const url = require('url');
 
-// establish db connection
-const Pool = pg.Pool;
-const pool = new Pool({
-    database: "task_app",
-    host: "localhost",
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000,
-});
+let config = {};
+
+if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: { rejectUnauthorized: false },
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+} else {
+    config = {
+        host: 'localhost', // Server hosting the postgres database
+        port: 5432, // env var: PGPORT
+        database: 'task_app', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+}
+
+const pool = new pg.Pool(config);
 
 // log connections to the database
 pool.on('connect', () => {
